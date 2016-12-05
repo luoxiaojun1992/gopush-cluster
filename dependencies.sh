0482d3198383d3a4b58120577d8bec6330fd490f
Gogs <gogitservice@gmail.com>
2016-12-05 23:48:36 +0800

    update dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Dependencies
 
-go get -u github.com/Terry-Mao/gopush-cluster
+go get -u github.com/luoxiaojun1992/gopush-cluster
 go get -u github.com/Terry-Mao/goconf
 go get -u github.com/garyburd/redigo/redis
 go get -u github.com/golang/net/websocket
 go get -u github.com/samuel/go-zookeeper
 go get -u github.com/alecthomas/log4go
 go get -u github.com/go-sql-driver/mysql
+go get -u github.com/influxdata/surgemq
 
 #go get -u ./message/...
 #go get -u ./comet/...
